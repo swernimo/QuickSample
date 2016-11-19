918acd274b143bd6cbb93f5cc6fb37b02417134d
Sean Wernimont <53341715@opayq.com>
2016-11-19 16:48:45 -0500

    added sample entity class and sample test class.

---
+++ QuickSample/QuickSample/SampleEntity.swift
@@ -0,0 +1,13 @@
+//
+//  SampleEntity.swift
+//  QuickSample
+//
+//  Created by Sean Wernimont on 11/19/16.
+//  Copyright © 2016 The Blind Squirrel. All rights reserved.
+//
+
+class SampleEntity{
+    func speak(_ word: String) -> String{
+        return "Hello \(word)"
+    }
+}
---
+++ QuickSample/QuickSampleTests/SampleEntityTests.swift
@@ -0,0 +1,27 @@
+//
+//  SampleEntityTests.swift
+//  QuickSample
+//
+//  Created by Sean Wernimont on 11/19/16.
+//  Copyright © 2016 The Blind Squirrel. All rights reserved.
+//
+
+import Quick
+import Nimble
+
+
+@testable import QuickSample
+
+class SampleEntityTests: QuickSpec {
+    
+    override func spec(){
+        describe("Sample Entity"){
+            it("speak should say Hello and append the supplied word"){
+                let entity = SampleEntity()
+                let expected = "Hello Jane"
+                let actual = entity.speak(word: "Jane")
+                expect(actual).to(equal(expected))
+            }
+        }
+    }
+}
